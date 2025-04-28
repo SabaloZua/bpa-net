3a41ee646720cc794172ccd1b06ccbc5be5e354e
SabaloZua <sabalozua@outlook.pt>
2025-04-28 22:37:19 +0100

    correção de erros no deploy

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url='http://localhost:5000'// 
+// https://bpanetapi.vercel.app   http://localhost:5000
 const api = axios.create({
   baseURL: 'https://bpanetapi.vercel.app', 
   headers: {
