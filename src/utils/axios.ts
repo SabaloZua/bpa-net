a8cd96307f390b4268df865830f1d79efa83f9cd
SabaloZua <sabalozua@outlook.pt>
2025-04-28 22:19:02 +0100

    Atualiza a URL base da API para produção

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+const url='https://bpanetapi.vercel.app'//http://localhost:5000
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Substitua pela URL da sua API
+  baseURL: url, 
   headers: {
     'Content-Type': 'application/json',
   },
