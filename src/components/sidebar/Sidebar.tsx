5329afa7b56e535a071b0a0f30ee7bb985aa256e
Sabalo Zua <sabalozua@outlook.pt>
2025-07-17 19:03:57 +0000

    upadate:add de scroll no navabar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -34,7 +34,7 @@ export default function Sidebar({ isOpen, handleLinkClick }: SidebarProps) {
     <>
       {/* Sidebar */}
       <div
-        className={`md:col-span-1 flex flex-col bg-blue-500 text-white fixed md:relative transition-all ease-in-out duration-300  ${isOpen ? "left-0" : "-left-full"} md:left-0 z-50 ${isOpen ? "min-h-screen" : "h-full"}`}
+        className={`  md:col-span-1 flex flex-col bg-blue-500 text-white fixed md:relative transition-all ease-in-out duration-300  ${isOpen ? "left-0" : "-left-full"} md:left-0 z-50 ${isOpen ? "min-h-screen" : "h-full"}`}
       >
         {/* Logo e Menu Hambúrguer */}
         <div className="p-4 flex justify-between items-center border-teal-700">
@@ -54,50 +54,51 @@ export default function Sidebar({ isOpen, handleLinkClick }: SidebarProps) {
      
         </div>
 
-        {/* Menu de Links */}
-        <div className={`${isOpen ? "block" : "hidden"} md:block`}>
-          <div className="p-2 text-sm font-medium">
-            <span>Menu</span>
-          </div>
+ 
+        {/* Menu de Links + Terminar Sessão com rolagem total */}
+<div className={`${isOpen ? "block" : "hidden"} md:block overflow-y-auto max-h-screen no-scrollbar flex flex-col`}>
+  <div className="p-2 text-sm font-medium">
+    <span>Menu</span>
+  </div>
 
-          <nav className="overflow-y-auto p-1">
-            {links.map((link: any, index: number) => (
-              <a
-                key={index}
-                className="btn"
-                data-active={link.active}
-                data-page={link.path}
-                type="button"
-                onClick={handleLinkClick}
-              >
-                <div
-                  className={`mx-4 my-2 p-3 rounded-md flex items-center justify-between cursor-pointer ${
-                    isActive(link.path) ? "bg-white text-black-2" : "hover:bg-blue-600"
-                  }`}
-                >
-                  <div className="flex items-center">
-                    <div className="mr-2">{link.icon}</div>
-                    <span className={`${isActive(link.path) ? "font-medium" : ""}`}>{link.name}</span>
-                  </div>
-                  {link.name !== "O Meu BPA" && <ChevronDown size={16} />}
-                </div>
-              </a>
-            ))}
-          </nav>
-
-          {/* Terminar Sessão */}
-          <div className="p-4 mt-auto">
-            <div className="flex items-center text-sm cursor-pointer">
-              <Link onClick={terminarSessao} href={"#"}>
-                <LogOut size={18} className="mr-2" />
-              </Link>
-              <div>
-                <div>Terminar Sessão</div>
-                <div className="text-xs">Expira em 20:42</div>
-              </div>
-            </div>
+  <nav className="p-1 flex-1">
+    {links.map((link: any, index: number) => (
+      <a
+        key={index}
+        className="btn"
+        data-active={link.active}
+        data-page={link.path}
+        type="button"
+        onClick={handleLinkClick}
+      >
+        <div
+          className={`mx-4 my-2 p-3 rounded-md flex items-center justify-between cursor-pointer ${
+            isActive(link.path) ? "bg-white text-black-2" : "hover:bg-blue-600"
+          }`}
+        >
+          <div className="flex items-center">
+            <div className="mr-2">{link.icon}</div>
+            <span className={`${isActive(link.path) ? "font-medium" : ""}`}>{link.name}</span>
           </div>
+          {link.name !== "O Meu BPA" && <ChevronDown size={16} />}
         </div>
+      </a>
+    ))}
+      <div className="p-4">
+    <div className="flex items-center text-sm cursor-pointer ">
+      <Link onClick={terminarSessao} href={"#"}>
+        <LogOut size={18} className="mr-2" />
+      </Link>
+      <div>
+        <div>Terminar Sessão</div>
+        <div className="text-xs">Expira em 20:42</div>
+      </div>
+    </div>
+  </div>
+  </nav>
+
+</div>
+
       </div>
     </>
   );
